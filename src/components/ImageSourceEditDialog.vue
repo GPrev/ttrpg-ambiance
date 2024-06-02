65184205e003e8795edeb9b7ad65a20b14754083
Gabriel Prevosto <prevosto.gabriel@neuf.fr>
2024-06-02 20:38:50 +0200

    Added editing and adding image sources

---
+++ src/components/ImageSourceEditDialog.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-dialog v-model="popupOpen">
+    <q-card style="min-width: 350px">
+      <q-card-section>
+        <div class="flex column" v-if="editedImage != null">
+          <ImageSourceView :image="editedImage" />
+          <q-input v-model="editedImage.name" label="Name" />
+          <q-input v-model="editedImage.url" label="URL" />
+        </div>
+      </q-card-section>
+      <q-card-actions align="right">
+        <q-btn flat label="Save" color="primary" @click="save" />
+        <q-btn flat label="Cancel" color="primary" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import ImageSourceView from './ImageSourceView.vue';
+import { ImageSource } from 'src/models/viewModels';
+import { useImagesStore } from 'src/stores/images-store';
+import { computed, ref, Ref } from 'vue';
+const imagesStore = useImagesStore();
+
+const editedImage: Ref<ImageSource | null> = ref(null);
+
+const popupOpen = computed({
+  get() {
+    return editedImage.value != null;
+  },
+  set(value: boolean) {
+    if (value && editedImage.value == null) {
+      editNew();
+    } else {
+      editedImage.value = null;
+    }
+  },
+});
+
+function editNew() {
+  editedImage.value = {
+    name: 'New Image',
+    key: '',
+    url: '',
+  };
+}
+
+function editExisting(image: ImageSource) {
+  editedImage.value = {
+    name: image.name,
+    key: image.key,
+    url: image.url,
+  };
+}
+
+function save() {
+  if (editedImage.value) {
+    if (editedImage.value?.key || '' != '') {
+      imagesStore.set(editedImage.value);
+    } else {
+      imagesStore.add(editedImage.value);
+    }
+  }
+  popupOpen.value = false;
+}
+
+defineExpose({ editNew, editExisting });
+</script>
--- src/pages/ImagesPage.vue
+++ src/pages/ImagesPage.vue
@@ -1,18 +1,39 @@
 <template>
   <q-page class="text-center q-pa-md flex flex-center">
     <q-spinner v-if="isLoading" color="primary" size="3em" />
-    <div v-if="!isLoading" class="q-pa-md row items-start q-gutter-md">
-      <q-card
-        v-for="[imageKey, image] of Object.entries(images)"
-        :key="imageKey"
-      >
-        <ImageSourceView :image="image" />
+    <div v-else>
+      <div class="q-pa-md row items-start q-gutter-md">
+        <q-card
+          v-for="[imageKey, image] of Object.entries(images)"
+          :key="imageKey"
+        >
+          <ImageSourceView :image="image" />
 
-        <q-card-section>
-          <div class="text-h6">{{ image.name }}</div>
-        </q-card-section>
-      </q-card>
+          <q-card-section>
+            <div class="text-h6">{{ image.name }}</div>
+          </q-card-section>
+          <q-btn
+            fab
+            icon="edit"
+            color="accent"
+            @click="
+              ($refs.editDialog as typeof ImageSourceEditDialog).editExisting(
+                image
+              )
+            "
+          />
+        </q-card>
+      </div>
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn
+          fab
+          icon="add"
+          color="accent"
+          @click="($refs.editDialog as typeof ImageSourceEditDialog).editNew()"
+        />
+      </q-page-sticky>
     </div>
+    <ImageSourceEditDialog ref="editDialog" />
   </q-page>
 </template>
 
@@ -20,6 +41,7 @@
 import ImageSourceView from 'src/components/ImageSourceView.vue';
 import { computed } from 'vue';
 import { useImagesStore } from 'src/stores/images-store';
+import ImageSourceEditDialog from 'src/components/ImageSourceEditDialog.vue';
 const imagesStore = useImagesStore();
 
 const images = computed(() => {
