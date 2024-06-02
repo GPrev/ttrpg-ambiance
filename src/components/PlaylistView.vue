ca795554e0350932bcd7b0f22dd9c346dd5ef3ee
Gabriel Prevosto <prevosto.gabriel@neuf.fr>
2024-06-02 21:12:56 +0200

    Fixed playlist view component

--- src/components/PlaylistView.vue
+++ src/components/PlaylistView.vue
@@ -1,8 +1,9 @@
 <template>
-  <AmbianceView :ambiance="props.playlist.ambiances[0]" />
+  <AmbianceView :ambiance="props.playlist.ambiances[0] || null" />
 </template>
 
 <script setup lang="ts">
+import AmbianceView from './AmbianceView.vue';
 import { Playlist } from 'src/models/viewModels';
 
 const props = defineProps<{
